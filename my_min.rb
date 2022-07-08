7389f77e4ddc16d6986e0a22f6e67759a6bf7763
Ari Moshe <arimoshe@gmail.com>
2022-07-08 12:16:09 -0700

    Finish Execution Time Diff

---
+++ my_min.rb
@@ -0,0 +1,49 @@
+def my_min1(arr)
+    min = arr[0]
+    arr.each_with_index do |ele1, i1|
+        arr.each_with_index do |ele2,i2|
+            if ele1 < ele2 && min > ele1
+                min = ele1 
+            elsif ele1 > ele2 && min > ele2
+                min = ele2
+            end
+        end
+
+    end
+    min
+end
+
+def largest_contiguous_subsum1(list)
+    subs = []
+    (0...list.length).each do |i|
+        (i...list.length).each do |j|
+                subs << list[i..j]            
+        end
+    end
+    
+    subs.map {|sub| sub.sum }.max
+end
+
+def largest_contiguous_subsum2(list)
+    current_sum = list.first
+    largest_sum = list.first
+    i = 1
+    while i < list.length
+        if current_sum < 0
+            current_sum = list[i]
+            largest_sum = current_sum if largest_sum < current_sum
+            i += 1
+        else
+            current_sum += list[i]
+            largest_sum = current_sum if largest_sum < current_sum
+            i += 1
+        end
+    end
+    largest_sum
+end
+
+
+
+    list = [-5,-50,-50,-2,-7,6]
+  p  largest_contiguous_subsum1(list) # => 8
+  p  largest_contiguous_subsum2(list) # => 8
